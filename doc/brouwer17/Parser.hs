1c91e6e49561a2d4a6bc32cdcde01961a841bcfc
gallais <guillaume.allais@ens-lyon.org>
2017-04-05 14:25:10 +0200

    [ doc ] Refactoring the Parser type

--- doc/brouwer17/Parser.hs
+++ doc/brouwer17/Parser.hs
@@ -5,32 +5,30 @@
 module Parser where
 
 import Data.Maybe
-import Data.Bifunctor (first)
 import Text.Read
 import Control.Applicative hiding (some, many)
 import Control.Monad (ap)
 
 newtype Parser a = Parser { runParser
   :: String        -- input string
-  -> [(a, String)] -- possible values + leftover
+  -> [(String, a)] -- possible values + leftover
   }
 
 parse :: Parser a -> String -> Maybe a
-parse p s = case filter (null . snd) (runParser p s) of
-    [(a,[])] -> Just a
+parse p s = case filter (null . fst) (runParser p s) of
+    [(_, a)] -> Just a
     _        -> Nothing
 
 anyChar :: Parser Char
 anyChar = Parser $ \ s -> case s of
   []      -> []
-  (c : s) -> [(c, s)]
+  (c : s) -> [(s, c)]
 
 guard :: (a -> Bool) -> Parser a -> Parser a
-guard f p = Parser $ filter (f . fst) . runParser p
+guard f p = Parser $ filter (f . snd) . runParser p
 
 guard' :: (a -> Maybe b) -> Parser a -> Parser b
-guard' f p = Parser $ catMaybes
-                    . fmap (\ (a,s) -> fmap (,s) (f a))
+guard' f p = Parser $ catMaybes . fmap (traverse f)
                     . runParser p
 
 digit :: Parser Char
@@ -40,15 +38,15 @@ digit' :: Parser Int
 digit' = guard' (readMaybe . (:[])) anyChar
 
 instance Functor Parser where
-  fmap f p = Parser $ fmap (first f) . runParser p
+  fmap f p = Parser $ fmap (fmap f) . runParser p
 
 instance Applicative Parser where
-  pure a = Parser $ \ s -> [(a,s)]
+  pure a = Parser $ \ s -> [(s,a)]
   (<*>)  = ap
 
 instance Monad Parser where
   return  = pure
-  p >>= f = Parser $ \ s -> runParser p s >>= \ (a, s') -> runParser (f a) s'
+  p >>= f = Parser $ \ s -> runParser p s >>= \ (s', a) -> runParser (f a) s'
 
 instance Alternative Parser where
   empty :: Parser a
@@ -62,7 +60,6 @@ some p = (:) <$> p <*> many p
 
 many :: Parser a -> Parser [a]
 many p = some p <|> pure []
-
 
 data Expr = Literal Int | Plus Expr Expr
   deriving (Show)
@@ -80,4 +77,3 @@ expr = Literal <$> int <|> Plus <$> expr <* char '+' <*> expr
 expr' :: Parser Expr
 expr' = base <|> Plus <$> base <* char '+' <*> expr'
     where base = Literal <$> int <|> char '(' *> expr' <* char ')'
-
