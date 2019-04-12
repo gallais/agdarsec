6383888a35b5d94c1ae0cf26b5ecd6ab0d1f6412
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-04-12 15:48:10 +0100

    [ travis ] build with Agda 2.6.0

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-VERSION=2.5.4.2
+VERSION=2.6.0
 
-if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "$VERSION" ]; then
+if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/Agda version \([0-9\.]+\)//"` = "$VERSION" ]; then
   cabal update
   cabal install alex happy cpphs
   cabal install Agda-${VERSION}
   mkdir -p $HOME/.agda
-  cp libraries-${VERSION} $HOME/.agda/
+  cp libraries $HOME/.agda/
   cd $HOME/.agda/
-  wget https://github.com/agda/agda-stdlib/archive/v0.17.tar.gz
-  tar -xvzf v0.17.tar.gz
+  wget https://github.com/agda/agda-stdlib/archive/v1.0.tar.gz
+  tar -xvzf v1.0.tar.gz
   cd -
 fi
