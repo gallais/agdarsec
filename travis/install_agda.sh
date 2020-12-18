14852509772bfe7c4a54c8c52baec1955131b45a
G. Allais <guillaume.allais@ens-lyon.org>
2020-12-18 10:38:57 +0000

    [ new ] runParserIO for frontend (#23)

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -13,6 +13,7 @@ rm -rf $HOME/.agda
 mkdir -p $HOME/.agda
 cp libraries $HOME/.agda/
 cd $HOME/.agda/
-wget https://github.com/agda/agda-stdlib/archive/v1.4.tar.gz
-tar -xvzf v1.4.tar.gz
+git clone --depth=1 https://github.com/agda/agda-stdlib
+# wget https://github.com/agda/agda-stdlib/archive/v1.4.tar.gz
+# tar -xvzf v1.4.tar.gz
 cd -
